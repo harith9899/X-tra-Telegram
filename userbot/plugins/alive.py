7aa1fb05747979a93b1abfcfcc2c0173bb6fa7ed
harith9899 <63388123+harith9899@users.noreply.github.com>
2020-04-11 12:28:18 +0300

    alive.py
    """Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
    import asyncio
    from telethon import events
    from telethon.tl.types import ChannelParticipantsAdmins
    from platform import uname
    from userbot import ALIVE_NAME
    from userbot.utils import admin_cmd
    
    DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @CcCYo"
    
    @command(outgoing=True, pattern="^.alivee$")
    async def amireallyalive(alive):
        """ For .alive command, check if the bot is running.  """
        await alive.edit("**YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ**\n\n"
                         "Telethon version: 6.9.0\nPython: 3.7.3\nfork by:@PPP_Q\n"
                         "Bot created by: [『ݲــٌـاࢪٽ آࢦـ نـَـࢩڪـل』](tg://user?id=992305036)\n"
                         "Database Status: Databases functioning normally!\n\nAlways with you, my master!\n"
                         f"My peru owner: {DEFAULTUSER}\n"
                         "[لصنع بوت راسلني](https://t.me/CcCYo)")

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -17,3 +17,22 @@ async def amireallyalive(alive):
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
                      f"`My peru owner`: {DEFAULTUSER}\n"
                      "[Deploy this userbot Now](https://github.com/Dark-Princ3/X-tra-Telegram)")
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from platform import uname
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @CcCYo"
+
+@command(outgoing=True, pattern="^.alivee$")
+async def amireallyalive(alive):
+    """ For .alive command, check if the bot is running.  """
+    await alive.edit("**YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ**\n\n"
+                     "Telethon version: 6.9.0\nPython: 3.7.3\nfork by:@PPP_Q\n"
+                     "Bot created by: [『ݲــٌـاࢪٽ آࢦـ نـَـࢩڪـل』](tg://user?id=992305036)\n"
+                     "Database Status: Databases functioning normally!\n\nAlways with you, my master!\n"
+                     f"My peru owner: {DEFAULTUSER}\n"
+                     "[لصنع بوت راسلني](https://t.me/CcCYo)"
