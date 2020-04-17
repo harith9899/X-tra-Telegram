2fde8c0ef254f2b7b08922fd4f134851fbbb8e9b
harith9899 <63388123+harith9899@users.noreply.github.com>
2020-04-17 08:00:35 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,19 +1,37 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
-import asyncio
-from telethon import events
-from telethon.tl.types import ChannelParticipantsAdmins
-from platform import uname
-from userbot import ALIVE_NAME
-from userbot.utils import admin_cmd
+"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""  
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @CcCYo"
+import asyncio  
 
-@command(outgoing=True, pattern="^.alivee$")
-async def amireallyalive(alive):
-    """ For .alive command, check if the bot is running.  """
-    await alive.edit("**YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ**\n\n"
-                     "Telethon version: 6.9.0\nPython: 3.7.3\nfork by:@PPP_Q\n"
-                     "Bot created by: [『ݲــٌـاࢪٽ آࢦـ نـَـࢩڪـل』](tg://user?id=992305036)\n"
-                     "Database Status: Databases functioning normally!\n\nAlways with you, my master!\n"
-                     f"My peru owner: {DEFAULTUSER}\n"
-                     "[لصنع بوت راسلني](https://t.me/CCCYO)")
+from telethon import events  
+
+from telethon.tl.types import ChannelParticipantsAdmins  
+
+from platform import uname  
+
+from userbot import ALIVE_NAME  
+
+from userbot.utils import admin_cmd  
+
+  
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @Pxx_x"  
+
+  
+
+@command(outgoing=True, pattern="^.alive$")  
+
+async def amireallyalive(alive):  
+
+    """ For .alive command, check if the bot is running.  """  
+
+    await alive.edit("**YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ**\n\n"  
+
+                     "Telethon version: 6.9.0\nPython: 3.7.3\nfork by: @CcCYo\n"  
+
+                     "Bot created by: [『ݲــٌـاࢪٽ آࢦـ نـَـࢩڪـل』 ](tg://user?id=992305036)\n"  
+
+                     "Database Status: Databases functioning normally!\n\nAlways with you, my master!\n"  
+
+                     f"My peru owner: {DEFAULTUSER}\n"  
+
+                     "[تابع جديدنا ليصلك كل ما هو جديد ♥️](https://t.me/joinchat/AAAAAEwLzFqiU7iYmuggWQ)")                  
